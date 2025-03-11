5c284709f76643e6ea1695676280f02b81da6286
Ashnc04 <s.lefebvre@cci-formation.nc>
2025-03-12 08:01:58 +1100

    changement convertisseur IP

--- scan_ip.ps1
+++ scan_ip.ps1
@@ -2,19 +2,25 @@
 $startIP = "10.13.0.50"
 $endIP = "10.13.0.75"
 
-# Convertir les adresses IP en objets IPAddress
-$startIPAddress = [System.Net.IPAddress]::Parse($startIP)
-$endIPAddress = [System.Net.IPAddress]::Parse($endIP)
+# Convertir les adresses IP en objets IPAddress et en entiers
+function ConvertTo-DecimalIP($ip) {
+    $bytes = ([System.Net.IPAddress]::Parse($ip)).GetAddressBytes()
+    [Array]::Reverse($bytes) # Convertir en big-endian pour correspondre à UInt32
+    return [System.BitConverter]::ToUInt32($bytes, 0)
+}
 
-# Obtenir les octets de l'adresse IP de début et de fin
-$startBytes = $startIPAddress.GetAddressBytes()
-$endBytes = $endIPAddress.GetAddressBytes()
+function ConvertTo-IPString($decimal) {
+    $bytes = [System.BitConverter]::GetBytes($decimal)
+    [Array]::Reverse($bytes) # Revenir en little-endian
+    return [System.Net.IPAddress]::new($bytes).ToString()
+}
+
+$startDecimal = ConvertTo-DecimalIP $startIP
+$endDecimal = ConvertTo-DecimalIP $endIP
 
 # Parcourir la plage d'adresses IP
-for ($i = [System.BitConverter]::ToUInt32($startBytes, 0); $i -le [System.BitConverter]::ToUInt32($endBytes, 0); $i++) {
-    # Convertir le nombre en adresse IP
-    $ipBytes = [System.BitConverter]::GetBytes($i)
-    $ip = [System.Net.IPAddress]::Parse([System.BitConverter]::ToString($ipBytes))
+for ($i = $startDecimal; $i -le $endDecimal; $i++) {
+    $ip = ConvertTo-IPString $i
 
     # Tester la connectivité avec Test-Connection
     if (Test-Connection -ComputerName $ip -Count 1 -Quiet) {
